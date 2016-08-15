b988cde23e3f86026dbc2a6be59e4873175f05f0
Jonathan Silva <silva.jon86@gmail.com>
2016-08-15 13:51:11 -0400

    testing testing

--- index.js
+++ index.js
@@ -1,9 +1,38 @@
-import $ from 'jquery';
-import data from './main'
-import {text} from './template';
+// import $ from 'jquery';
+// import data from './main'
+// import {text} from './template';
+
+var data = [
+  { type: 'text', label: 'Name' },
+  { type: 'email', label: 'Email' },
+  { type: 'password', label: 'Password' },
+  { type: 'submit', label: 'Register'}
+];
+
+
+function text(inputInfo) {
+	return `
+	<div class="text_div">
+		<input type="${inputInfo.type}" placeholder="${inputInfo.label}">
+	</div>
+	`;
+}
+
+
+function button(inputInfo) {
+	console.log(inputInfo);
+	return ` 
+	<div class="button_div">
+		<button type="${inputInfo.type}" placeholder="${inputInfo.label}"></button>
+	</div>
+	`;
+}
 
 var dataTypeToTemplate = {
-	'text': text
+	'text': text,
+	'email': text,
+	'password': text,
+	'submit': button
 }
 
 data.forEach(function(inputInfo) {
